730a7274581e6aae210721026a7881ff262aa9cc
asahi417 <asahi1992ushio@gmail.com>
2022-11-14 08:16:17 +0000

    add qa-based evaluation

---
+++ misc/qag_model_evaluation/dataset_stats.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from datasets import load_dataset
+
+LMS = ["t5-small-squad",
+       "t5-base-squad",
+       "t5-large-squad",
+       "t5-small-squad-multitask",
+       "t5-base-squad-multitask",
+       "t5-large-squad-multitask",
+       "bart-base-squad",
+       "bart-large-squad"]
+DOMAINS = ["new_wiki", "reditt", "amazon", "nyt"]
+
+table = []
+for lm in LMS:
+    for d in DOMAINS:
+        data = load_dataset("lmqg/qa_squadshifts_pseudo", f"{d}.{lm}")
+        table.append({"lm": lm, "domain": d, "train": len(data["train"]), "validation": len(data["validation"]), "test": len(data["test"])})
+    pd.DataFrame(table).to_csv('summary.dataset.csv', index=False)
