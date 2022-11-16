8c37b15b288e307e3544412f03159a32e4c9611a
asahi417 <asahi1992ushio@gmail.com>
2022-11-16 11:39:11 +0000

    add decoder perplexity of seq2seq model

--- misc/qag_end2end_model/model_finetuning.sh
+++ misc/qag_end2end_model/model_finetuning.sh
@@ -1,6 +1,6 @@
 
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-small" -b 64 -g 1 2 -c "lmqg_output/t5_small_tweetqa" -i 'paragraph' -o 'questions_answers' -p 'qag' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
-lmqg-eval -m "lmqg_output/t5_small_tweetqa/best_model" -e "lmqg_output/t5_small_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --prediction-aggregation "first" --prediction-level "sentence" --max-length-output 128 --max-length 256
+lmqg-eval -m "lmqg_output/t5_small_tweetqa/best_model" -e "lmqg_output/t5_small_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --prediction-aggregation "first" --prediction-level "paragraph" --max-length-output 128 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/t5_small_tweetqa/best_model" -a "t5-small-tweetqa-qag" -o "lmqg"
 
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-base"  -b 64 -g 1 2 -c "lmqg_output/t5_base_tweetqa" -i 'paragraph' -o 'questions_answers' -p 'qag' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
