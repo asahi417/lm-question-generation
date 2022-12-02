7b98a4999ceb0d4ea4ec0db0c5d10296aba2c6fe
asahi417 <asahi1992ushio@gmail.com>
2022-12-02 16:07:56 +0000

    add bert answer extractor

--- lmqg/data.py
+++ lmqg/data.py
@@ -3,6 +3,7 @@ import logging
 import os
 import requests
 from os.path import join as pj
+from glob import glob
 
 from datasets import load_dataset
 from .language_model import internet_connection
@@ -19,48 +20,63 @@ def get_dataset(path: str = 'lmqg/qg_squad',
                 output_type: str = 'question',
                 use_auth_token: bool = False):
     """ Get question generation input/output list of texts. """
-    if name == 'default':
-        dataset = load_dataset(path, split=split, use_auth_token=use_auth_token)
-    else:
-        dataset = load_dataset(path, name, split=split, use_auth_token=use_auth_token)
+    name = None if name == 'default' else name
+    dataset = load_dataset(path, name, split=split, use_auth_token=use_auth_token)
     return dataset[input_type], dataset[output_type]
 
 
 def get_reference_files(path: str = 'lmqg/qg_squad', name: str = 'default', cache_dir: str = None):
     """ Get reference files for automatic evaluation """
     url = f'https://huggingface.co/datasets/{path}/raw/main/reference_files'
+    name = None if name == 'default' else name
     local_files_only = not internet_connection()
     if cache_dir is None:
         cache_dir = pj(DEFAULT_CACHE_DIR, 'reference_files', path)
     output = {}
     for split in ['test', 'validation']:
+        dataset = load_dataset(path, name, split=split)
         for feature in ['answer', 'question', 'paragraph', 'sentence', 'questions_answers']:
+            if feature not in dataset:
+                continue
             if name == 'default':
                 filename = f'{feature}-{split}.txt'
             else:
                 filename = f'{feature}-{split}.{name}.txt'
-            path = pj(cache_dir, filename)
-            if os.path.exists(path):
-                with open(path) as f:
+            ref_path = pj(cache_dir, filename)
+            if os.path.exists(ref_path):
+                with open(ref_path) as f:
                     line_length = len(f.read().split('\n'))
                 if line_length < 20:
-                    os.remove(path)
-            if not os.path.exists(path):
+                    os.remove(ref_path)
+            if not os.path.exists(ref_path):
                 if local_files_only:
                     logging.info(f'network is not reachable, could not download the file from {url}/{filename}')
                     continue
-                os.makedirs(os.path.dirname(path), exist_ok=True)
-                with open(path, "wb") as f:
+                os.makedirs(os.path.dirname(ref_path), exist_ok=True)
+                try:
                     r = requests.get(f'{url}/{filename}')
                     content = r.content
-                    if "Entry not found" in str(content):
-                        continue
-                    f.write(content)
-            with open(path) as f:
+                    assert "Entry not found" not in str(content)
+                    with open(ref_path, "wb") as f:
+                        f.write(content)
+                except Exception:
+                    logging.warning(f'can not get file from {url}/{filename}. generate reference file instead')
+                    generate_reference_files(dataset=dataset, ref_path=ref_path, feature=feature)
+
+            with open(ref_path) as f:
                 line_length = len(f.read().split('\n'))
             if line_length < 20:
-                os.remove(path)
+                os.remove(ref_path)
                 continue
-            assert os.path.exists(path)
-            output['{}-{}'.format(feature, split)] = path
+            assert os.path.exists(ref_path)
+            output[f'{feature}-{split}'] = ref_path
     return output
+
+
+def generate_reference_files(dataset, ref_path: str, feature: str):
+    with open(ref_path, 'w') as f:
+        if feature == 'paragraph':
+            tmp_data = dataset['paragraph_id']
+        else:
+            tmp_data = dataset[feature]
+        f.write('\n'.join([i.replace('\n', '.') for i in tmp_data]))
