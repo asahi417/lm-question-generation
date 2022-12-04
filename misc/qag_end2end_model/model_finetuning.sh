eab812996a0c5c8849e043b51d3e4ae3de027098
asahi417 <asahi1992ushio@gmail.com>
2022-12-04 12:31:42 +0000

    add bert answer extractor

--- misc/qag_end2end_model/model_finetuning.sh
+++ misc/qag_end2end_model/model_finetuning.sh
@@ -1,41 +1,42 @@
 # With prefix
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-small" -b 64 -g 1 2 -c "lmqg_output/t5_small_tweetqa" -i 'paragraph' -o 'questions_answers' -p 'qag' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
 lmqg-eval -m "lmqg_output/t5_small_tweetqa/best_model" -e "lmqg_output/t5_small_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/t5_small_tweetqa/best_model" -e "lmqg_output/t5_small_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/t5_small_tweetqa/best_model" -a "t5-small-tweetqa-qag" -o "lmqg"
 
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-base"  -b 32 -g 2 4 -c "lmqg_output/t5_base_tweetqa" -i 'paragraph' -o 'questions_answers' -p 'qag' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
 lmqg-eval -m "lmqg_output/t5_base_tweetqa/best_model" -e "lmqg_output/t5_base_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/t5_base_tweetqa/best_model" -e "lmqg_output/t5_base_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/t5_base_tweetqa/best_model" -a "t5-base-tweetqa-qag" -o "lmqg"
 
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-large" -b 16 -g 4 8 -c "lmqg_output/t5_large_tweetqa" -i 'paragraph' -o 'questions_answers' -p 'qag' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
 lmqg-eval -m "lmqg_output/t5_large_tweetqa/best_model" -e "lmqg_output/t5_large_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/t5_large_tweetqa/best_model" -e "lmqg_output/t5_large_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/t5_large_tweetqa/best_model" -a "t5-large-tweetqa-qag" -o "lmqg"
 
-# Without prefix
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "facebook/bart-base" -b 32 -g 4 8 -c "lmqg_output/bart_base_tweetqa" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
 lmqg-eval -m "lmqg_output/bart_base_tweetqa/best_model" -e "lmqg_output/bart_base_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/bart_base_tweetqa/best_model" -e "lmqg_output/bart_base_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/bart_base_tweetqa/best_model" -a "bart-base-tweetqa-qag" -o "lmqg"
-
-lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-small" -b 64 -g 1 2 -c "lmqg_output/t5_small_tweetqa_np" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
-lmqg-eval -m "lmqg_output/t5_small_tweetqa_np/best_model" -e "lmqg_output/t5_small_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
-lmqg-push-to-hf -m "lmqg_output/t5_small_tweetqa_np/best_model" -a "t5-small-tweetqa-qag-np" -o "lmqg"
-
-lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-base"  -b 32 -g 2 4 -c "lmqg_output/t5_base_tweetqa_np" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
-lmqg-eval -m "lmqg_output/t5_base_tweetqa_np/best_model" -e "lmqg_output/t5_base_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
-lmqg-push-to-hf -m "lmqg_output/t5_base_tweetqa_np/best_model" -a "t5-base-tweetqa-qag-np" -o "lmqg"
-
-lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-large" -b 16 -g 4 8 -c "lmqg_output/t5_large_tweetqa_np" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
-lmqg-eval -m "lmqg_output/t5_large_tweetqa_np/best_model" -e "lmqg_output/t5_large_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
-lmqg-push-to-hf -m "lmqg_output/t5_large_tweetqa_np/best_model" -a "t5-large-tweetqa-qag-np" -o "lmqg"
 
 lmqg-train-search -d "lmqg/qag_tweetqa" -m "facebook/bart-large" -b 32 -g 4 8 -c "lmqg_output/bart_large_tweetqa" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
 lmqg-eval -m "lmqg_output/bart_large_tweetqa/best_model" -e "lmqg_output/bart_large_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/bart_large_tweetqa/best_model" -e "lmqg_output/bart_large_tweetqa/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/bart_large_tweetqa/best_model" -a "bart-large-tweetqa-qag" -o "lmqg"
 
-# extra evaluation
-for LM in "bart-base-tweetqa-qag" "bart-large-tweetqa-qag" "t5-small-tweetqa-qag" "t5-base-tweetqa-qag" "t5-large-tweetqa-qag" "t5-small-tweetqa-qag-np" "t5-base-tweetqa-qag-np" "t5-large-tweetqa-qag-np"
-do
-  git clone "https://huggingface.co/lmqg/${LM}"
-  lmqg-eval -m "${LM}" -e "${LM}/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --prediction-level "answer" --max-length-output 128 --max-length 256
-  lmqg-push-to-hf -m "${LM}" -a "${LM}" -o "lmqg"
-done
+# Without prefix
+
+lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-small" -b 64 -g 1 2 -c "lmqg_output/t5_small_tweetqa_np" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
+lmqg-eval -m "lmqg_output/t5_small_tweetqa_np/best_model" -e "lmqg_output/t5_small_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/t5_small_tweetqa_np/best_model" -e "lmqg_output/t5_small_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
+lmqg-push-to-hf -m "lmqg_output/t5_small_tweetqa_np/best_model" -a "t5-small-tweetqa-qag-np" -o "research-backup"
+
+lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-base"  -b 32 -g 2 4 -c "lmqg_output/t5_base_tweetqa_np" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
+lmqg-eval -m "lmqg_output/t5_base_tweetqa_np/best_model" -e "lmqg_output/t5_base_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/t5_base_tweetqa_np/best_model" -e "lmqg_output/t5_base_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
+lmqg-push-to-hf -m "lmqg_output/t5_base_tweetqa_np/best_model" -a "t5-base-tweetqa-qag-np" -o "research-backup"
+
+lmqg-train-search -d "lmqg/qag_tweetqa" -m "t5-large" -b 16 -g 4 8 -c "lmqg_output/t5_large_tweetqa_np" -i 'paragraph' -o 'questions_answers' --epoch-partial 10 -e 15 --max-length-output-eval 128 --max-length-output 128 --max-length-eval 256 --max-length 256
+lmqg-eval -m "lmqg_output/t5_large_tweetqa_np/best_model" -e "lmqg_output/t5_large_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" -i "paragraph" -o 'questions_answers' --max-length-output 128 --max-length 256
+lmqg-eval-qag -m "lmqg_output/t5_large_tweetqa_np/best_model" -e "lmqg_output/t5_large_tweetqa_np/best_model/eval" --language "en" -d "lmqg/qag_tweetqa" --max-length-output 128 --max-length 256
+lmqg-push-to-hf -m "lmqg_output/t5_large_tweetqa_np/best_model" -a "t5-large-tweetqa-qag-np" -o "research-backup"
--- misc/qag_multitask_model/eval_aligned_f1.sh
+++ misc/qag_multitask_model/eval_aligned_f1.sh
@@ -5,7 +5,7 @@ qag_metric () {
   DATA=${2}
   LA=${3}
   git clone "https://huggingface.co/lmqg/${MODEL}"
-  lmqg-eval-qag --use-auth-token -m "lmqg/${MODEL}" -e "${MODEL}/eval" -d "${DATA}" --language "${LA}"
+#  lmqg-eval-qag --use-auth-token -m "lmqg/${MODEL}" -e "${MODEL}/eval" -d "${DATA}" --language "${LA}"
   lmqg-push-to-hf -m "${MODEL}" -a "${MODEL}" -o "lmqg"
 }
 
@@ -16,6 +16,14 @@ qag_metric 't5-large-squad-qg' 'lmqg/qg_squad' "en"
 qag_metric 'bart-base-squad-qg' 'lmqg/qg_squad' "en"
 qag_metric 'bart-large-squad-qg' 'lmqg/qg_squad' "en"
 
+qag_metric 'mbart-large-cc25-jaquad-qg' 'lmqg/qg_jaquad' "ja"
+qag_metric 'mbart-large-cc25-esquad-qg' 'lmqg/qg_esquad' "es"
+qag_metric 'mbart-large-cc25-dequad-qg' 'lmqg/qg_dequad' "de"
+qag_metric 'mbart-large-cc25-itquad-qg' 'lmqg/qg_itquad' "it"
+qag_metric 'mbart-large-cc25-koquad-qg' 'lmqg/qg_koquad' "ko"
+qag_metric 'mbart-large-cc25-ruquad-qg' 'lmqg/qg_ruquad' "ru"
+qag_metric 'mbart-large-cc25-frquad-qg' 'lmqg/qg_frquad' "fr"
+
 qag_metric 'mt5-small-jaquad-qg' 'lmqg/qg_jaquad' "ja"
 qag_metric 'mt5-small-esquad-qg' 'lmqg/qg_esquad' "es"
 qag_metric 'mt5-small-dequad-qg' 'lmqg/qg_dequad' "de"
@@ -24,6 +32,7 @@ qag_metric 'mt5-small-koquad-qg' 'lmqg/qg_koquad' "ko"
 qag_metric 'mt5-small-ruquad-qg' 'lmqg/qg_ruquad' "ru"
 qag_metric 'mt5-small-frquad-qg' 'lmqg/qg_frquad' "fr"
 
+# TODO
 qag_metric 'mt5-base-jaquad-qg' 'lmqg/qg_jaquad' "ja"
 qag_metric 'mt5-base-esquad-qg' 'lmqg/qg_esquad' "es"
 qag_metric 'mt5-base-dequad-qg' 'lmqg/qg_dequad' "de"
@@ -31,12 +40,3 @@ qag_metric 'mt5-base-itquad-qg' 'lmqg/qg_itquad' "it"
 qag_metric 'mt5-base-koquad-qg' 'lmqg/qg_koquad' "ko"
 qag_metric 'mt5-base-ruquad-qg' 'lmqg/qg_ruquad' "ru"
 qag_metric 'mt5-base-frquad-qg' 'lmqg/qg_frquad' "fr"
-
-
-qag_metric 'mbart-large-cc25-jaquad-qg' 'lmqg/qg_jaquad' "ja"
-qag_metric 'mbart-large-cc25-esquad-qg' 'lmqg/qg_esquad' "es"
-qag_metric 'mbart-large-cc25-dequad-qg' 'lmqg/qg_dequad' "de"
-qag_metric 'mbart-large-cc25-itquad-qg' 'lmqg/qg_itquad' "it"
-qag_metric 'mbart-large-cc25-koquad-qg' 'lmqg/qg_koquad' "ko"
-qag_metric 'mbart-large-cc25-ruquad-qg' 'lmqg/qg_ruquad' "ru"
-qag_metric 'mbart-large-cc25-frquad-qg' 'lmqg/qg_frquad' "fr"
