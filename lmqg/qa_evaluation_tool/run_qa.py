7846aadc0310c688b56d0dbbf8c1e2f4f45b80a8
asahi417 <asahi1992ushio@gmail.com>
2022-12-21 23:03:38 +0900

    add pipeline QAG

--- lmqg/qa_evaluation_tool/run_qa.py
+++ lmqg/qa_evaluation_tool/run_qa.py
@@ -268,18 +268,18 @@ def run_qa_evaluation(dataset: str,
     # Create train feature from dataset
 
     train_example = raw_datasets[split_train].filter(lambda x: len(x['question']) < max_seq_length)
-    for examples in train_example:
-        print(examples)
-        tokenizer(
-            examples[question_column_name if pad_on_right else context_column_name],
-            examples[context_column_name if pad_on_right else question_column_name],
-            truncation="only_second" if pad_on_right else "only_first",
-            max_length=max_seq_length,
-            stride=doc_stride,
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            padding="max_length"
-        )
+    # for examples in train_example:
+    #     print(examples)
+    #     tokenizer(
+    #         examples[question_column_name if pad_on_right else context_column_name],
+    #         examples[context_column_name if pad_on_right else question_column_name],
+    #         truncation="only_second" if pad_on_right else "only_first",
+    #         max_length=max_seq_length,
+    #         stride=doc_stride,
+    #         return_overflowing_tokens=True,
+    #         return_offsets_mapping=True,
+    #         padding="max_length"
+    #     )
 
     train_dataset = train_example.map(
         prepare_train_features, batched=True, num_proc=None,
@@ -297,9 +297,7 @@ def run_qa_evaluation(dataset: str,
 
     # Validation Feature Creation
     validation_example = raw_datasets[split_validation].filter(lambda x: len(x['question']) < max_seq_length)
-    if down_sample_size_validation is not None:
-        assert down_sample_size_validation < len(validation_example), \
-            f"{down_sample_size_validation} should be less than {len(validation_example)}"
+    if down_sample_size_validation is not None and down_sample_size_validation < len(validation_example):
         validation_example = validation_example.shuffle(random_seed)
         validation_example = validation_example.select(list(range(down_sample_size_validation)))
     validation_dataset = validation_example.map(
