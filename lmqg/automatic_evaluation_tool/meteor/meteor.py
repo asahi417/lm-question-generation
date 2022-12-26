104bfc8694e8919762c0ff169669214e1b92723e
asahi417 <asahi1992ushio@gmail.com>
2022-12-26 10:05:09 +0900

    add pipeline QAG

--- lmqg/automatic_evaluation_tool/meteor/meteor.py
+++ lmqg/automatic_evaluation_tool/meteor/meteor.py
@@ -76,15 +76,17 @@ class Meteor:
         with self.lock:
             assert len(hyps) == len(refs), f"{len(hyps)} != {len(refs)}"
             for h, r in zip(hyps, refs):
-                print(h, r)
                 h = h.encode() if type(h) is str else h
                 r = r if type(r) is list else [r]
                 r = [_r.encode() if type(_r) is str else _r for _r in r]
                 if self.normalize_hypothesis:
                     h = text_normalization(h.decode()).encode('utf-8')
-                print(h, r)
-                input()
+
+
                 stat = self._stat(h, r)
+                print(h, r)
+                print(stat)
+                input()
                 eval_line += ' ||| {}'.format(stat)
 
             self.meteor_p.stdin.write(enc('{}\n'.format(eval_line)))
