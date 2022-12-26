1e89924c81c937029e7f41dc43dd1be3982c3c4c
asahi417 <asahi1992ushio@gmail.com>
2022-12-26 15:59:11 +0900

    add pipeline QAG

--- lmqg/lmqg_cl/readme_template.py
+++ lmqg/lmqg_cl/readme_template.py
@@ -439,7 +439,8 @@ def get_readme(model_name: str, model_checkpoint: str):
     if os.path.exists(tmp_path):
         with open(tmp_path) as f:
             metric_qag_pipeline = {k: {_k: round(_v * 100, 2) for _k, _v in v.items()} for k, v in json.load(f).items()}
-
+    print(tmp_path, os.path.exists(tmp_path))
+    input()
     metric_main = [dataset, dataset_name, metric, metric_qag, metric_qa, metric_ae, metric_qag_pipeline]
 
     # metric for ood
