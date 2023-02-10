426d353cef724f1ab1c07ecd8a9e1a4f6d4080c5
asahi417 <asahi1992ushio@gmail.com>
2023-02-10 14:28:34 +0000

    fix push_to_hub

--- lmqg/lmqg_cl/push_to_hf.py
+++ lmqg/lmqg_cl/push_to_hf.py
@@ -11,7 +11,41 @@ import transformers
 from lmqg.lmqg_cl.readme_template import get_readme
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-
+gitattribute = """*.7z filter=lfs diff=lfs merge=lfs -text
+*.arrow filter=lfs diff=lfs merge=lfs -text
+*.bin filter=lfs diff=lfs merge=lfs -text
+*.bz2 filter=lfs diff=lfs merge=lfs -text
+*.ckpt filter=lfs diff=lfs merge=lfs -text
+*.ftz filter=lfs diff=lfs merge=lfs -text
+*.gz filter=lfs diff=lfs merge=lfs -text
+*.h5 filter=lfs diff=lfs merge=lfs -text
+*.joblib filter=lfs diff=lfs merge=lfs -text
+*.lfs.* filter=lfs diff=lfs merge=lfs -text
+*.mlmodel filter=lfs diff=lfs merge=lfs -text
+*.model filter=lfs diff=lfs merge=lfs -text
+*.msgpack filter=lfs diff=lfs merge=lfs -text
+*.npy filter=lfs diff=lfs merge=lfs -text
+*.npz filter=lfs diff=lfs merge=lfs -text
+*.onnx filter=lfs diff=lfs merge=lfs -text
+*.ot filter=lfs diff=lfs merge=lfs -text
+*.parquet filter=lfs diff=lfs merge=lfs -text
+*.pb filter=lfs diff=lfs merge=lfs -text
+*.pickle filter=lfs diff=lfs merge=lfs -text
+*.pkl filter=lfs diff=lfs merge=lfs -text
+*.pt filter=lfs diff=lfs merge=lfs -text
+*.pth filter=lfs diff=lfs merge=lfs -text
+*.rar filter=lfs diff=lfs merge=lfs -text
+*.safetensors filter=lfs diff=lfs merge=lfs -text
+saved_model/**/* filter=lfs diff=lfs merge=lfs -text
+*.tar.* filter=lfs diff=lfs merge=lfs -text
+*.tflite filter=lfs diff=lfs merge=lfs -text
+*.tgz filter=lfs diff=lfs merge=lfs -text
+*.wasm filter=lfs diff=lfs merge=lfs -text
+*.xz filter=lfs diff=lfs merge=lfs -text
+*.zip filter=lfs diff=lfs merge=lfs -text
+*.zst filter=lfs diff=lfs merge=lfs -text
+*tfevents* filter=lfs diff=lfs merge=lfs -text
+"""
 
 def main():
     parser = argparse.ArgumentParser(description='Push to Model hub')
@@ -28,7 +62,7 @@ def main():
     logging.info(f"Upload {opt.model_checkpoint} to {opt.organization}/{opt.model_alias}")
 
     # url = create_repo(opt.model_alias, organization=opt.organization, exist_ok=True)
-    url = create_repo(repo_id=f"{opt.organization}/{opt.model_alias}", exist_ok=True, repo_type="model")
+    create_repo(repo_id=f"{opt.organization}/{opt.model_alias}", exist_ok=True, repo_type="model")
 
     if not opt.skip_model_upload:
         tokenizer = transformers.AutoTokenizer.from_pretrained(opt.model_checkpoint, local_files_only=True)
@@ -53,7 +87,6 @@ def main():
         tokenizer.push_to_hub(**args)
         config.push_to_hub(**args)
 
-
     # upload remaining files
     copy_tree(f"{opt.model_checkpoint}", f"{opt.model_alias}")
 
@@ -66,7 +99,9 @@ def main():
         f.write(readme)
 
     # upload remaining files
-    copy_tree(f"{opt.model_checkpoint}", f"{opt.model_alias}")
+    copy_tree(opt.model_checkpoint, opt.model_alias)
+    with open(f"{opt.model_alias}/.gitattributes", 'w') as f:
+        f.write(gitattribute)
     os.system(
-        f"cd {opt.model_alias} && git lfs install && git add . && git commit -m 'model update' && git push && cd ../")
-    shutil.rmtree(f"{opt.model_alias}")  # clean up the cloned repo
+        f"cd {opt.model_alias} && git lfs install && git lfs track  && git add . && git commit -m 'model update' && git push && cd ../")
+    shutil.rmtree(opt.model_alias)  # clean up the cloned repo
