31800542bf58e6408fe9716fc75bc659911f7607
asahi417 <asahi1992ushio@gmail.com>
2023-02-10 10:26:45 +0000

    update DockerFile

--- misc/flan_t5.sh
+++ misc/flan_t5.sh
@@ -1,5 +1,3 @@
-
-
 # Pipe
 lmqg-train-search -m "google/flan-t5-large" -b 4 -g 16 32 -c "lmqg_output/flan-t5-large-squad-ae" -i 'paragraph_sentence' -o 'answer' -p 'ae'
 lmqg-eval -m "lmqg_output/flan-t5-large-squad-ae/best_model" -e "lmqg_output/flan-t5-large-squad-ae/best_model/eval" --language "en" -d "lmqg/qg_squad" -i "paragraph_sentence" -o 'answer'
@@ -39,14 +37,22 @@ lmqg-eval-qag -m "lmqg_output/flan-t5-small-squad-qg-ae/best_model" -e "lmqg_out
 lmqg-push-to-hf -m "lmqg_output/flan-t5-small-squad-qg-ae/best_model" -a "flan-t5-small-squad-qg-ae" -o "lmqg"
 
 # QG
+lmqg-train-search -m "google/flan-t5-large" -p qg -b 16 -g 4 8 16 32 -c "lmqg_output/flan-t5-large-squad"
+lmqg-eval -m "lmqg_output/flan-t5-large-squad/best_model" -e "lmqg_output/flan-t5-large-squad/best_model/eval" --language "en" -d "lmqg/qg_squad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
+lmqg-push-to-hf -m "lmqg_output/flan-t5-large-squad/best_model" -a "flan-t5-large-squad-qg" -o "lmqg"
+
+lmqg-train-search -m "google/flan-t5-base" -p qg -b 16 -g 4 8 16 32 -c "lmqg_output/flan-t5-base-squad"
+lmqg-eval -m "lmqg_output/flan-t5-base-squad/best_model" -e "lmqg_output/flan-t5-base-squad/best_model/eval" --language "en" -d "lmqg/qg_squad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
+lmqg-push-to-hf -m "lmqg_output/flan-t5-base-squad/best_model" -a "flan-t5-base-squad-qg" -o "lmqg"
+
+lmqg-train-search -m "google/flan-t5-small" -p qg -b 16 -g 4 8 16 32 -c "lmqg_output/flan-t5-small-squad"
+lmqg-eval -m "lmqg_output/flan-t5-small-squad/best_model" -e "lmqg_output/flan-t5-small-squad/best_model/eval" --language "en" -d "lmqg/qg_squad" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
+lmqg-push-to-hf -m "lmqg_output/flan-t5-small-squad/best_model" -a "flan-t5-small-squad-qg" -o "lmqg"
+
 evaluate () { # CKPT LANG DATA ALIAS
   lmqg-eval -m "lmqg_output/${1}/best_model" -e "lmqg_output/${1}/best_model/eval" --language "${2}" -d "lmqg/${3}" -i "paragraph_answer" --prediction-aggregation "first" --prediction-level "sentence"
   lmqg-push-to-hf -m "lmqg_output/${1}/best_model" -a "${4}" -o "lmqg"
-  }
-
-
-lmqg-train-search -m google/flan-t5-large -p qg -b 16 -g 4 8 16 32 -c lmqg_output/flan_t5_large_squad
-evaluate flan_t5_large_squad en qg_squad flan-t5-large-squad-qg
+}
 lmqg-train-search -m google/flan-t5-base -p qg -b 16 -g 4 8 16 32 -c lmqg_output/flan_t5_base_squad
 evaluate flan_t5_base_squad en qg_squad flan-t5-base-squad-qg
 lmqg-train-search -m google/flan-t5-small -p qg -b 32 -g 2 4 8 16 -c lmqg_output/flan_t5_small_squad
@@ -64,3 +70,9 @@ lmqg-push-to-hf -m "lmqg_output/flan-t5-base-squad-qag/best_model" -a "flan-t5-b
 lmqg-train-search -d "lmqg/qag_squad" -m "google/flan-t5-large" -b 2 -g 32 64 -c "lmqg_output/flan-t5-large-squad-qag" -i 'paragraph' -o 'questions_answers' -p 'qag' --n-max-config 2 --epoch-partial 5 -e 15 --max-length-output-eval 256 --max-length-output 256
 lmqg-eval -m "lmqg_output/flan-t5-large-squad-qag/best_model" -e "lmqg_output/flan-t5-large-squad-qag/best_model/eval" --language "en" -d "lmqg/qag_squad" -i "paragraph" -o 'questions_answers' --max-length-output 256 --max-length 256
 lmqg-push-to-hf -m "lmqg_output/flan-t5-large-squad-qag/best_model" -a "flan-t5-large-squad-qag" -o "lmqg"
+
+# pipeline
+git clone "https://huggingface.co/lmqg/flan-t5-small-squad-qg"
+lmqg-eval-qag -m "flan-t5-small-squad-qg" --model-ae "lmqg/flan-t5-small-squad-ae" -e "flan-t5-small-squad-qg/eval_pipeline" -d "lmqg/qg_squad" --language "en"
+lmqg-push-to-hf -m "flan-t5-small-squad-qg" -a "flan-t5-small-squad-qg" -o "lmqg"
+
