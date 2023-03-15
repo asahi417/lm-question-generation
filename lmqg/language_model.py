0b57a8daf330e3363705c462871b4327d8fe7156
asahi417 <asahi1992ushio@gmail.com>
2023-03-15 21:50:13 +0000

    fix trimmer

--- lmqg/language_model.py
+++ lmqg/language_model.py
@@ -230,6 +230,10 @@ class EncodePlus:
                     if not self.drop_overflow_error_text:  # raise error for overflow text
                         raise ExceedMaxLengthError(self.max_length)
                     return None  # remove overflow text
+        print(input_sequence)
+        print(self.param_in)
+        input()
+
         encode = self.tokenizer(text_target=input_sequence, **self.param_in)
         if output_sequence is not None:
             encode['labels'] = self.tokenizer.encode(output_sequence, **self.param_out)
